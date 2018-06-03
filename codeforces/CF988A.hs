9ccca8b538c5c72c105c551c94a113aa3f9dbb86
Hao Tang <tanghaoth90@gmail.com>
2018-06-03 21:08:32 +0800

    haskell program

---
+++ codeforces/CF988A.hs
@@ -0,0 +1,26 @@
+import Data.Map (Map)
+import Data.List
+import qualified Data.Map as M
+import Control.Monad
+
+readInts :: IO [Int]
+readInts = fmap (map read.words) getLine
+
+getmsg k msize
+  | msize >= k  = "YES"
+  | otherwise   = "NO"
+
+list_as_str :: [Int] -> String
+list_as_str ilist = (concat $ intersperse " " (map show ilist)) 
+  
+main = do
+  nkline <- readInts
+  numlist <- readInts
+  let n = head nkline
+  let k = last nkline
+  let nilist = zip numlist [1..n]
+  let mp = M.fromList nilist
+  let msg = getmsg k (M.size $ mp)
+  putStrLn msg
+  when (msg == "YES") $ 
+    do putStrLn (list_as_str (snd (unzip (take k (M.toList(mp))))))
